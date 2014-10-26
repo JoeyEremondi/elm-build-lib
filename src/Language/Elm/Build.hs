db3f2fd5604547051293146727dd7db34e90f2fd
Joey Eremondi <jmitdase@gmail.com>
2014-10-26 13:43:13 -0700

    Added compileInOrder function

--- src/Language/Elm/Build.hs
+++ src/Language/Elm/Build.hs
@@ -1,5 +1,7 @@
 module Language.Elm.Build where
 
+import qualified Data.Map as Map
+
 import Elm.Compiler
 import Elm.Compiler.Module
 
@@ -9,3 +11,15 @@ elmModuleName :: String -> Either String Elm.Compiler.Module.Name
 elmModuleName modul = do
     (name, _) <- parseDependencies modul
     return name
+
+compileInOrder :: [String] -> Either String String
+compileInOrder modules = helper modules Map.empty ""
+    where
+      helper [] _ src = return src
+      helper (modul:otherModules) compiledModules _ = do
+          name <- elmModuleName modul
+          (newModule, src) <- compile modul compiledModules
+          let newCompiled = Map.insert name newModule compiledModules
+          helper otherModules newCompiled src   
+          
+    
