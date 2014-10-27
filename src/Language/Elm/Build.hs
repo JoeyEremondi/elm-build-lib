9ef72cbca15e9824930c8444f6473c95775a6f14
Joey Eremondi <jmitdase@gmail.com>
2014-10-27 06:51:50 -0700

    Added topSort function

--- src/Language/Elm/Build.hs
+++ src/Language/Elm/Build.hs
@@ -4,13 +4,61 @@ import qualified Data.Map as Map
 
 import Elm.Compiler
 import Elm.Compiler.Module
+import qualified Data.Set as Set
+import qualified Data.Map as Map
 
-x=3
+import Control.Monad (filterM)
+
 
 elmModuleName :: String -> Either String Elm.Compiler.Module.Name
 elmModuleName modul = do
     (name, _) <- parseDependencies modul
     return name
+
+
+
+--Reorder the dependencies so that we can compile them in order
+resolveDependnecies :: [String] -> Either String [Name]
+resolveDependnecies deps = do
+    edgePairs <- mapM parseDependencies deps
+    let names = map fst edgePairs
+    let edgeMap = Map.fromList edgePairs
+    --Get predecessors of each node, needed for the top sort
+
+    let eitherLookUp :: Name -> Map.Map Name [Name] -> Either String [Name] 
+        eitherLookUp name map = case Map.lookup name map of
+                Nothing -> Left $  "Dependency not found: " ++ nameToString name
+                Just b -> Right b
+    let n1 `hasEdgeFrom` n2 = case Map.lookup n2 edgeMap of
+                Nothing -> False
+                Just l -> n1 `elem` l
+
+    let predMap = Map.fromList $ zip names $ map (\end -> [start | start <- names, end `hasEdgeFrom` start ]) names
+    let sources = filter (null . snd) edgePairs
+    let initialVisitedNodes = Set.fromList $ map fst sources
+    let initialOpenList = map fst sources
+    let initialResult = map fst sources
+    
+    
+    let seenAllPred :: Set.Set Name -> Name -> Either String Bool
+        seenAllPred visited name = do
+                predList <- eitherLookUp name predMap
+                let unseenPreds = filter (not . (flip Set.member $ visited)) predList
+                return $ null unseenPreds
+    let topSort _ [] result = return result
+        topSort visited (current:otherNodes) resultSoFar = do
+                currentEdges <- eitherLookUp current edgeMap
+                let alreadySeen = filter (flip Set.member $ visited) currentEdges
+                case alreadySeen of
+                    [] -> Left "Error: you have a dependency cycle. Your program cannot be compiled"
+                    _ -> do
+                        let newVisited = Set.insert current visited
+                        newSources <- filterM (seenAllPred newVisited) currentEdges
+                        topSort newVisited (newSources ++ otherNodes) (resultSoFar ++ newSources)
+
+    topSort initialVisitedNodes initialOpenList initialResult
+                        
+        
 
 compileInOrder :: [String] -> Either String String
 compileInOrder modules = helper modules Map.empty ""
