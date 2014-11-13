ccb85e2eea142761116813b9431f3278aa2f0dab
jmite <jmitdase@gmail.com>
2014-11-13 17:14:26 +0100

    Started including stdlib

--- src/Language/Elm/CoreLibs.hs
+++ src/Language/Elm/CoreLibs.hs
@@ -1,6 +1,44 @@
  {-# LANGUAGE TemplateHaskell #-}
-module Language.Elm.CoreLibs where
-
-import Data.FileEmbed
-
-arraySrc =  $(embedFile  "core/src/Array.elm")
+ module Language.Elm.CoreLibs where
+   
+   import Data.FileEmbed
+   
+   sources =  [$(embedFile  "core/src/Array.elm")
+   ,$(embedFile  "core/src/Array.elm")
+   ,$(embedFile  "core/src/Basics.elm")
+   ,$(embedFile  "core/src/Bitwise.elm")
+   ,$(embedFile  "core/src/Char.elm")
+   ,$(embedFile  "core/src/Color.elm")
+   ,$(embedFile  "core/src/Date.elm")
+   ,$(embedFile  "core/src/Debug.elm")
+   ,$(embedFile  "core/src/Dict.elm")
+   ,$(embedFile  "core/src/Http.elm")
+   ,$(embedFile  "core/src/Keyboard.elm")
+   ,$(embedFile  "core/src/List.elm")
+   ,$(embedFile  "core/src/Maybe.elm")
+   ,$(embedFile  "core/src/Mouse.elm")
+   ,$(embedFile  "core/src/Random.elm")
+   ,$(embedFile  "core/src/Regex.elm")
+   ,$(embedFile  "core/src/Result.elm")
+   ,$(embedFile  "core/src/Set.elm")
+   ,$(embedFile  "core/src/Signal.elm")
+   ,$(embedFile  "core/src/String.elm")
+   ,$(embedFile  "core/src/Text.elm")
+   ,$(embedFile  "core/src/Time.elm")
+   ,$(embedFile  "core/src/Touch.elm")
+   ,$(embedFile  "core/src/Trampoline.elm")
+   ,$(embedFile  "core/src/Transform2D.elm")
+   ,$(embedFile  "core/src/WebSocket.elm")
+   ,$(embedFile  "core/src/Window.elm")
+   
+   ,$(embedFile  "core/src/Json/Encode.elm")
+   ,$(embedFile  "core/src/Json/Decode.elm")
+   
+   ,$(embedFile  "core/src/Graphics/Collage.elm")
+   ,$(embedFile  "core/src/Graphics/Element.elm")
+   ,$(embedFile  "core/src/Graphics/Input.elm")
+   
+   ,$(embedFile  "core/src/Graphics/Input/Field.elm")
+   ]
+   
+   --jsSources = 
